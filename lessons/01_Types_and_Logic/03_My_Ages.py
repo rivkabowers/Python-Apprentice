af6f48851e97db362d78f04db74c1919f2deb4e4
rivkabowers <bowersrivka69@gmail.com>
2024-10-13 18:28:49 +0000

    elif else

--- lessons/01_Types_and_Logic/03_My_Ages.py
+++ lessons/01_Types_and_Logic/03_My_Ages.py
@@ -8,9 +8,9 @@ tell the user what age groups the user is in. The groups are:
 0-2: Baby
 3-5: Toddler
 6-12: Child
-13-19: Teen
-20-64: Adult
-65+: Senior
+13-18: Teen
+19-54: Adult
+54+: Senior
 
 Except, if the user is the same age as you, print "You are pretty awesome!"
 
@@ -31,20 +31,29 @@ messagebox.showinfo('What you are', "You are a baby.")
 
 """
 
-from tkinter import messagebox, simpledialog, Tk # import required modules
+from tkinter import messagebox, simpledialog, Tk ;age = simpledialog.askinteger("Your Age", "How old are you?")
 
 window = Tk()     # Create a window object
 window.withdraw() # Hide the window; we just want to see pop ups
 
-# Ask the user's age
+messagebox.showinfo('How old are you')# Ask the user's age
 
+if age >=0 and age <=2: 
+    messagebox.showinfo('What you are', 'You are a Baby')
+if age >=3 and age <=5: 
+    messagebox.showinfo('What you are', 'You are a Toddler')
+if age >=6 and age <=12: 
+    messagebox.showinfo('What you are', 'You are a Child')
+if age >=13 and age <=18: 
+    messagebox.showinfo('What you are', 'You are a Teen')
+if age >=19 and age <=54: 
+    messagebox.showinfo('What you are', 'You are a Adult')
+if age >=54 and age <=1000: 
+    messagebox.showinfo('What you are', 'You are a Senior')
 # Use if statements to determine the age group
 # and create a message
 
 # Show the message to the user
-
-
-
 window.mainloop()  # Keeps the window open
 
 
