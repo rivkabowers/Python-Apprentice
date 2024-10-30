e020407c15c6480801f8b3002fb08871fd15f6ee
rivkabowers <bowersrivka69@gmail.com>
2024-10-30 02:09:33 +0000

    fix story

--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -7,11 +7,11 @@ append them to the story
 """
 
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
-    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
+    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽', 'they']
 
 story = []
 
-story = story + words[0:1] + words[7:8] + words[1:2] + words[-9:-8] + words[-4:-3]+ words[7:8] + words[3:4] + words[-5:-4] + words[-10:-9] + words[8:9] + words[4:5] + words[6:7] + words[-7:-6] + words[-9:-8] + words[-4:-3]+ words[7:8] + words[-8:-7] + ['.'] + ['there'] + words[19:20] + ['met'] + words[7:8] + words[1:2] + words[-9:-8] + words[-4:-3]+ words[7:8] + words[-8:-7] + words[-6:-5] + words[19:20] + ['played'] + words[6:7] + words[7:8] + words[18:19] + ['together'] + ['.']
+story = story + words[-10:-9] + words[8:9] + words[4:5] + words[6:7] + words[-7:-6] + words[-9:-8] + words[-4:-3]+ words[7:8] + words[-8:-7] +['.'] + [('there')] + words[19:20] + [('met')] + words[7:8] + words[1:2] + words[-9:-8] + words[-4:-3]+ words[7:8] + words[-8:-7] + words[-6:-5] + words[19:20] + [('played')] + words[6:7] + words[7:8] + words[18:19] + [('together')] + [('.')]
 #there they met a boy with a cat and they played with a ball together
 # Create a story using the words in the list
 
