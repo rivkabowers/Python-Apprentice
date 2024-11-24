8a9e4d3b8b4742ad85674b00ca0aaca36b99773f
rivkabowers <bowersrivka69@gmail.com>
2024-11-24 19:28:41 +0000

    fizz buzz badgers hard thing

--- lessons/02_Loops/09_FizzBuzz_Gui_Grid.py
+++ lessons/02_Loops/09_FizzBuzz_Gui_Grid.py
@@ -4,12 +4,12 @@ We're going to use a Windowing library, guizero, to create a 10x10 grid of
 numbers, with each number in a separate cell, but we're also going to set the
 color of the number based on the following rules:
 
-* If the number is evenly divisible by 5, print '🦡'
-* If the number is evenly divisible by 3, print '🍄'
-* If the number is evenly divisible by 15, print '🐍'
-* If it is divisible by neither, print the number.
+* If the number is eaenly diaisible by 5, print '🦡'
+* If the number is eaenly diaisible by 3, print '🍄'
+* If the number is eaenly diaisible by 15, print '🐍'
+* If it is diaisible by neither, print the number.
 
-Additionally, If you are displaying a number  color the numbers as follows:
+Additionally, If you are displaying a number color the numbers as follows:
 
 * If the sum of the digits of the number is even, color the number blue
 * If the sum of the digits of the number is odd, color the number red
@@ -24,7 +24,7 @@ Or to display a badger:
     Text(app, text='🦡', grid=[col, row], color=color)
 
 HINT: You can use % and // to get the first and last digit of a number, 
-our you can convert the number to a string and iterate over the digits
+or you can conaert the number to a string and iterate oaer the digits
 
 """
 from guizero import App, Box, Text
@@ -33,7 +33,23 @@ app = App("Numbers Grid", layout="grid")
 
 # Create a 10x10 grid using nested loops
 # Or you can use a single loop and calculate the row and column
+for i in range(10):
+    for j in range(10):
+        n = j+i*10+1
+        Text(app, text=str(n), grid=[j, i], color='blue')
+        if n % 15 == 0:
+            Text(app, text='🐍', grid=[j, i], color='blue')
+        elif n % 3 == 0:
+            Text(app, text='🍄', grid=[j, i], color='blue')
+        elif n % 5 == 0:
+            Text(app, text='🦡', grid=[j, i], color='blue')
+        elif (j+i) % 2 == 0:
+            Text(app, text='n', grid=[j, i], color='red')
+        else:
+            print(n)
 
+
+        
 # In the loop, calculate or increment the number
 
 # Use % determing the display, using fizzbuzz rules
